c2415ca16ffb532fc739002caba2eae644535900
whrksp126 <whrksp126@gmail.com>
2026-07-31 18:33:49 +0900

    fix: target daemon device migration table

--- codingpt_back/migrations/20260731083000-add-daemon-device-alias.js
+++ codingpt_back/migrations/20260731083000-add-daemon-device-alias.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('daemon_devices', 'device_alias', {
+    await queryInterface.addColumn('daemon_device', 'device_alias', {
       type: Sequelize.STRING(40),
       allowNull: true,
     });
   },
 
   async down(queryInterface) {
-    await queryInterface.removeColumn('daemon_devices', 'device_alias');
+    await queryInterface.removeColumn('daemon_device', 'device_alias');
   },
 };
